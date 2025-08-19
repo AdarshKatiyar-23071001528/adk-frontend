b13822cf6ebcacf95d5e156d57bfbae701dbfc2b
Adarsh <adarshkatiyar73@gmail.com>
2025-08-19 16:08:17 +0530

    enhance banner again

--- src/Components/Product/Banner.jsx
+++ src/Components/Product/Banner.jsx
@@ -3,7 +3,7 @@ import Bubble from '../../Common/Bubble';
 
 const Banner = () => {
   return (
-    <div className='w-full h-[230px] md:h-[330px] flex items-center justify-center p-2 md:p-6 relative'>
+    <div className='w-full h-[200px] md:h-[330px] flex items-center justify-center p-2 md:p-6 relative'>
       <div className='w-full h-full  p-1 md:p-2 flex items-center justify-center rounded-xl relative'>
             {/* <img src="https://www.shutterstock.com/image-vector/25-rupee-off-sale-discount-260nw-2142090371.jpg" className='w-full h-full object-contains rounded-xl' alt="" /> */}
             <div className='w-full h-full bg-linear rounded-xl flex text-pretty flex-col p-1 md:p-3 shadow-b-lg border relative overflow-hidden'>
@@ -11,7 +11,7 @@ const Banner = () => {
                 <h1 className='font-bold text-center text-xl md:text-6xl p-1 md:p-3'>Big Deal</h1>
                 <h2 className='text-center font-bold text-xl md:text-3xl p-1 md:p-3 text-'>Only 25</h2>
                 <h3 className='text-center font-bold text-xl md:text-3xl p-1 md:p-3 text-gray-600'>150 <span className='text-red-600'>%</span> Earning</h3>
-                <button className='p-2 bg-blue-400 w-[10%] rounded-xl font-bold shadow-lg absolute bottom-0 left-0'>Order Now</button>
+                <button className='p-2 bg-blue-400 w-fit  rounded-xl font-bold shadow-lg absolute bottom-2 left-2'>Order Now</button>
                   <Bubble/>
             </div>
       </div>
